12a8c6a66dd669d68c459eca8243a6aef8ecb7ea
derkdts <160583121+derkdts@users.noreply.github.com>
2025-01-26 22:00:50 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import streamlit as st
 import pandas as pd
 
 # Создаем образец данных
-data = {'Имя': ['Аня', 'Борис', 'Вера'], 'Город': ['Москва', 'Питер', 'Екатеринбург']}
+data = {'Имя': ['Алуа', 'Ерлан', 'Ернар', 'Максат', 'Рамазан', 'Рамазан']}
 df = pd.DataFrame(data)
 
 # Создаем список вариантов для выпадающего меню
-options = ['Да', 'Нет', 'Может быть']
+options = ['Спланк', 'Тикет', 'АЦ']
 
 # Создаем функцию для обновления данных в таблице
 def update_df(new_value):
-    df.loc[selected_index, 'Новый столбец'] = new_value
+    df.loc[selected_index, 'Канал'] = new_value
     st.dataframe(df)
 
 # Создаем интерфейс Streamlit
@@ -20,7 +20,7 @@ st.title('Таблица с выпадающим меню')
 st.dataframe(df)
 
 # Добавляем новый столбец с выпадающим меню
-df['Новый столбец'] = ''
+df['Канал'] = ''
 selected_index = st.number_input('Выберите индекс строки', min_value=0, max_value=len(df)-1, value=0)
 new_value = st.selectbox('Выберите значение', options)
 st.button('Обновить', on_click=update_df, args=(new_value,))
