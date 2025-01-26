4c5463da9461f366bcc1a5e6cd8675a0a9829460
derkdts <160583121+derkdts@users.noreply.github.com>
2025-01-26 21:55:14 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-# Создаем DataFrame (пример)
-data = {'Имя': ['Алиса', 'Борис', 'Вера'],
-        'Возраст': [25, 30, 22],
-        'Город': ['Москва', 'Санкт-Петербург', 'Новосибирск']}
+# Создаем образец данных
+data = {'Имя': ['Аня', 'Борис', 'Вера'], 'Город': ['Москва', 'Питер', 'Екатеринбург']}
 df = pd.DataFrame(data)
 
-# Заголовок приложения
-st.title('Пример таблицы в Streamlit')
+# Создаем список вариантов для выпадающего меню
+options = ['Да', 'Нет', 'Может быть']
+
+# Создаем функцию для обновления данных в таблице
+def update_df(new_value):
+    df.loc[selected_index, 'Новый столбец'] = new_value
+    st.dataframe(df)
+
+# Создаем интерфейс Streamlit
+st.title('Таблица с выпадающим меню')
 
 # Отображаем таблицу
-st.table(df)
+st.dataframe(df)
+
+# Добавляем новый столбец с выпадающим меню
+df['Новый столбец'] = ''
+selected_index = st.number_input('Выберите индекс строки', min_value=0, max_value=len(df)-1, value=0)
+new_value = st.selectbox('Выберите значение', options)
+st.button('Обновить', on_click=update_df, args=(new_value,))
