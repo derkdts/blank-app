6ac5bab749793af66b4cda5bc3f2f20566bbe1b9
derkdts <160583121+derkdts@users.noreply.github.com>
2025-01-26 22:30:24 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,29 +3,29 @@ import pandas as pd
 
 def create_table_with_dropdown(data, options):
     df = pd.DataFrame(data)
-    df['Канал'] = ''
+    df['Обед'] = ''  # Добавляем новый столбец "Обед"
     selected_values = []
 
     st.title('Распределение')
 
     for index, row in df.iterrows():
-        # Добавляем порядковый номер для пользователей с одинаковыми именами
         unique_identifier = f"{row['Имя']} ({index+1})"
-        selected_values.append(st.selectbox(f"Выбор для {unique_identifier}", options))
+        selected_values.append(st.selectbox(f"Время обеда для {unique_identifier}", options))
 
     def update_df():
         for i in range(len(df)):
             new_value = selected_values[i]
-            df.loc[i, 'Канал'] = new_value
+            df.loc[i, 'Обед'] = new_value
         st.dataframe(df)
 
     st.button('Обновить', on_click=update_df)
 
     return df
 
-# Пример использования
+# Данные для таблиц
 data = {'Имя': ['Алуа', 'Ерлан', 'Ернар', 'Максат', 'Рамазан']}
-options = ['Спланк', 'Тикет', 'АЦ']
+options = ['12:00', '13:00', '14:00', '15:00']
+
 df = create_table_with_dropdown(data, options)
 
 # Сохранение результатов (пример)
